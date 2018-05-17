46e4b8549eeacdced172a2c10fe12a386c3346a3
Jirffey Gabuya <boyhimas@gmail.com>
2018-05-18 00:05:44 +0800

    Additional scene skeletons

---
+++ frontend/src/scenes/Transactions/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Transactions extends React.Component {
+    render() {
+        return (
+            <h2>Transactions</h2>
+        )
+    }
+}
+
+export default Transactions;
