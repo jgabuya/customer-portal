5b3a7999c83637d483c07b7cbd182d46483b33dd
Jirffey Gabuya <boyhimas@gmail.com>
2018-05-18 00:30:01 +0800

    404 scene skeleton

---
+++ frontend/src/scenes/NotFound/index.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class NotFound extends Component {
+    render() {
+        return (
+            <h2>404 - Page Not Found</h2>
+        )
+    }
+}
+
+export default NotFound;
