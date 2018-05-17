2687fa97830182f33a7dd7fcbc0a3ef74cdb415b
Jirffey Gabuya <boyhimas@gmail.com>
2018-05-18 00:05:12 +0800

    Dashboard scene

---
+++ frontend/src/scenes/Dashboard/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Dashboard extends React.Component {
+    render() {
+        return (
+            <h2>Dashboard</h2>
+        )
+    }
+}
+
+export default Dashboard;
