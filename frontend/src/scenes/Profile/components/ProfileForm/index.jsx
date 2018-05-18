703db2387db9576c1d81f91d5e68bcbbe0f21ed4
Jirffey Gabuya <boyhimas@gmail.com>
2018-05-19 01:57:57 +0800

    Add profile form

---
+++ frontend/src/scenes/Profile/components/ProfileForm/index.jsx
@@ -0,0 +1,80 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {Button, Col, Form, FormGroup, FormText, Input, Label} from 'reactstrap';
+
+class ProfileForm extends Component {
+    render() {
+        return (
+            <Form onSubmit={this.props.onHandleSubmit}>
+                <FormGroup row>
+                    <Label for="customerId" md={3}>Customer Id</Label>
+
+                    <Col sm={9}>
+                        <Input type="text" name="customerId" id="customerId" value={this.props.customerId} placeholder="Customer Id" disabled />
+                    </Col>
+                </FormGroup>
+
+                <FormGroup row>
+                    <Label for="email" md={3}>Email Address</Label>
+
+                    <Col sm={9}>
+                        <Input type="email" name="email" id="email" value={this.props.email} required placeholder="Email Address" />
+                    </Col>
+                </FormGroup>
+
+                <FormGroup row>
+                    <Label for="firstName" md={3}>First Name</Label>
+
+                    <Col sm={9}>
+                        <Input type="text" name="firstName" id="firstName" value={this.props.firstName} required placeholder="First Name" />
+                    </Col>
+                </FormGroup>
+
+                <FormGroup row>
+                    <Label for="lastName" md={3}>Last Name</Label>
+
+                    <Col sm={9}>
+                        <Input type="text" name="lastName" id="lastName" value={this.props.lastName} required placeholder="Last Name" />
+                    </Col>
+                </FormGroup>
+
+                <FormGroup row>
+                    <Label for="phone" md={3}>Phone Number</Label>
+
+                    <Col sm={9}>
+                        <Input type="text" name="phone" id="phone" value={this.props.phone} required placeholder="Phone Number" />
+                    </Col>
+                </FormGroup>
+
+                <FormGroup row>
+                    <Label for="address" md={3}>Address</Label>
+
+                    <Col sm={9}>
+                        <Input type="textarea" name="address" id="address" value={this.props.address} required placeholder="Address" />
+                        <FormText>
+                            For the sake of brevity, this field will also accept City, Province/State, and Country for now
+                        </FormText>
+                    </Col>
+                </FormGroup>
+
+                <FormGroup row>
+                    <Col sm={{ size: 3, offset: 3 }}>
+                        <Button color="primary" block>Submit</Button>
+                    </Col>
+                </FormGroup>
+            </Form>
+        )
+    }
+}
+
+ProfileForm.propTypes = {
+    customerId: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    firstName: PropTypes.string.isRequired,
+    lastName: PropTypes.string.isRequired,
+    phone: PropTypes.string.isRequired,
+    address: PropTypes.string.isRequired,
+    onHandleSubmit: PropTypes.func.isRequired
+};
+
+export default ProfileForm;
--- frontend/src/scenes/Profile/index.jsx
+++ frontend/src/scenes/Profile/index.jsx
@@ -1,9 +1,59 @@
 import React, {Component} from 'react';
+import {Col, Row} from 'reactstrap';
+
+import ProfileForm from './components/ProfileForm';
 
 class Profile extends Component {
+    constructor(props) {
+        super(props);
+
+        // initialize state
+        this.state = {
+            customerId: '',
+            email: '',
+            firstName: '',
+            lastName: '',
+            phone: '',
+            address: ''
+        };
+
+        this.onHandleFormSubmit = this.onHandleFormSubmit.bind(this);
+    }
+
+    componentDidMount() {
+        this.setState({
+            customerId: '792c02b1-e230-4946-83bd-d4588095b501',
+            email: 'sherlock@mail.com',
+            firstName: 'Sherlock',
+            lastName: 'Holmes',
+            phone: '223-222-2222',
+            address: '221B Baker Street, London, UK'
+        })
+    }
+
+    onHandleFormSubmit(e) {
+        e.preventDefault();
+        const data = new FormData(e.target);
+
+        console.log(data.get('email'));
+    }
+
     render() {
         return (
-            <h2>Profile</h2>
+            <div>
+                <Row>
+                    <Col>
+                        <h2>Profile</h2>
+                        <hr/>
+                    </Col>
+                </Row>
+
+                <Row className="mt-3">
+                    <Col md={{ size: 10, offset: 1 }}>
+                        <ProfileForm {...this.state} onHandleSubmit={this.onHandleFormSubmit} />
+                    </Col>
+                </Row>
+            </div>
         )
     }
 }
