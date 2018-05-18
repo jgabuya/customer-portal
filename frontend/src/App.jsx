0e125e95ec2c6c47b3a32975149bbbbb1c2ca013
Jirffey Gabuya <boyhimas@gmail.com>
2018-05-18 22:16:52 +0800

    Minor structural changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,47 +25,45 @@ class App extends Component {
                     <title>Home</title>
                 </Helmet>
 
-                <Router>
-                    <div>
-                        <MainNav />
+                <MainNav />
 
-                        <main role="main" className="container">
-                            {/*
+                <Router>
+                    <main role="main" className="container">
+                        {/*
                             Define routes using react-router. Throughout the whole app, we are using Dynamic instead of
                             Static Routing, aka "routing that takes place as your app is rendering, not in a
                             configuration or convention outside of a running app". Head over to the following link for more
                             info: https://reacttraining.com/react-router/web/guides/philosophy
                         */}
 
-                            <Switch>
-                                {/*
+                        <Switch>
+                            {/*
                                 Taken from react-router docs:
                                 The Route component is perhaps the most important component in React Router to
                                 understand and learn to use well. Its most basic responsibility is to render some
                                 UI when a location matches the route’s path.
                                 https://reacttraining.com/react-router/web/api/Route
                             */}
-                                <Route exact path="/" component={Dashboard}/>
-                                <Route path="/profile" component={Profile}/>
-                                <Route path="/transactions" component={Transactions}/>
-                                <Route path="/login" component={Login}/>
+                            <Route exact path="/" component={Dashboard}/>
+                            <Route path="/profile" component={Profile}/>
+                            <Route path="/transactions" component={Transactions}/>
+                            <Route path="/login" component={Login}/>
 
-                                {/* 404 route, default if none of the above route/crumbroutes are satisfied */}
-                                <Route path="/404" component={NotFound}/>
-                                <Redirect to="/404"/>
-                            </Switch>
+                            {/* 404 route, default if none of the above route/crumbroutes are satisfied */}
+                            <Route path="/404" component={NotFound}/>
+                            <Redirect to="/404"/>
+                        </Switch>
 
-                            <footer>
-                                <hr/>
+                        <footer>
+                            <hr/>
 
-                                <div className="row">
-                                    <div className="col-md-12">
-                                        &copy; {new Date().getFullYear()} Customer Portal
-                                    </div>
+                            <div className="row">
+                                <div className="col-md-12">
+                                    &copy; {new Date().getFullYear()} Customer Portal
                                 </div>
-                            </footer>
-                        </main>
-                    </div>
+                            </div>
+                        </footer>
+                    </main>
                 </Router>
             </div>
         );
