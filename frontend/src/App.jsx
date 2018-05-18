5a9b6e55721a65d8df9db6902c4a24fdf5510a39
Jirffey Gabuya <boyhimas@gmail.com>
2018-05-18 22:14:20 +0800

    Formatting

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import React, {Component} from "react";
-import {Link, BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
+import {BrowserRouter as Router, Redirect, Route, Switch} from "react-router-dom";
 import {Helmet} from "react-helmet";
 
 // load global css first before loading components that have their own css references
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 import './App.css';
+
+import MainNav from './components/MainNav';
 
 import Login from './scenes/Login';
 import Dashboard from './scenes/Dashboard';
@@ -22,44 +26,46 @@ class App extends Component {
                 </Helmet>
 
                 <Router>
-                    <MainNav />
+                    <div>
+                        <MainNav />
 
-                    <main role="main" className="container">
-                        {/*
+                        <main role="main" className="container">
+                            {/*
                             Define routes using react-router. Throughout the whole app, we are using Dynamic instead of
                             Static Routing, aka "routing that takes place as your app is rendering, not in a
                             configuration or convention outside of a running app". Head over to the following link for more
                             info: https://reacttraining.com/react-router/web/guides/philosophy
                         */}
 
-                        <Switch>
-                            {/*
+                            <Switch>
+                                {/*
                                 Taken from react-router docs:
                                 The Route component is perhaps the most important component in React Router to
                                 understand and learn to use well. Its most basic responsibility is to render some
                                 UI when a location matches the route’s path.
                                 https://reacttraining.com/react-router/web/api/Route
                             */}
-                            <Route exact path="/" component={Dashboard}/>
-                            <Route path="/profile" component={Profile}/>
-                            <Route path="/transactions" component={Transactions}/>
-                            <Route path="/login" component={Login}/>
+                                <Route exact path="/" component={Dashboard}/>
+                                <Route path="/profile" component={Profile}/>
+                                <Route path="/transactions" component={Transactions}/>
+                                <Route path="/login" component={Login}/>
 
-                            {/* 404 route, default if none of the above route/crumbroutes are satisfied */}
-                            <Route path="/404" component={NotFound}/>
-                            <Redirect to="/404"/>
-                        </Switch>
+                                {/* 404 route, default if none of the above route/crumbroutes are satisfied */}
+                                <Route path="/404" component={NotFound}/>
+                                <Redirect to="/404"/>
+                            </Switch>
 
-                        <footer>
-                            <hr/>
+                            <footer>
+                                <hr/>
 
-                            <div className="row">
-                                <div className="col-md-12">
-                                    &copy; {new Date().getFullYear()} EQX
+                                <div className="row">
+                                    <div className="col-md-12">
+                                        &copy; {new Date().getFullYear()} Customer Portal
+                                    </div>
                                 </div>
-                            </div>
-                        </footer>
-                    </main>
+                            </footer>
+                        </main>
+                    </div>
                 </Router>
             </div>
         );
