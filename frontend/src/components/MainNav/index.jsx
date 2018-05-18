dee76137443117b279b51d2179e00de56dc006bd
Jirffey Gabuya <boyhimas@gmail.com>
2018-05-18 22:09:32 +0800

    Minor changes

--- frontend/src/components/MainNav/index.jsx
+++ frontend/src/components/MainNav/index.jsx
@@ -1,17 +1,14 @@
 import React, {Component} from 'react';
 import {
-    Button,
     Collapse,
     DropdownMenu,
     DropdownItem,
     DropdownToggle,
-    Form,
-    Input,
-    InputGroup,
-    InputGroupAddon,
     Navbar,
     NavbarToggler,
     NavbarBrand,
+    NavItem,
+    NavLink,
     Nav,
     UncontrolledDropdown
 } from 'reactstrap';
@@ -37,70 +34,32 @@ class MainNav extends Component {
             <div>
                 <Navbar color="dark" dark fixed="top" expand="md">
                     <div className="container">
-                        <NavbarBrand href="/">EQX</NavbarBrand>
+                        <NavbarBrand href="/">Customer Portal</NavbarBrand>
                         <NavbarToggler onClick={this.toggle} />
 
                         <Collapse isOpen={this.state.isOpen} navbar>
                             {/* left menu */}
                             <Nav navbar>
-                                <UncontrolledDropdown>
-                                    <DropdownToggle nav caret>
-                                        Listing
-                                    </DropdownToggle>
+                                <NavItem>
+                                    <NavLink href="/">Dashboard</NavLink>
+                                </NavItem>
 
-                                    <DropdownMenu>
-                                        <DropdownItem href="/investor/listing/subscriptions">My Subscriptions</DropdownItem>
-                                        <DropdownItem href="/investor/listing/cipo/list">CIPOs</DropdownItem>
-                                    </DropdownMenu>
-                                </UncontrolledDropdown>
-
-                                <UncontrolledDropdown>
-                                    <DropdownToggle nav caret>
-                                        Trading
-                                    </DropdownToggle>
-
-                                    <DropdownMenu>
-                                        <DropdownItem href="/investor/trading/markets">Markets</DropdownItem>
-                                        <DropdownItem href="/investor/trading/watchlist">Watchlist</DropdownItem>
-                                        <DropdownItem href="/investor/trading/portfolio">Portfolio</DropdownItem>
-                                        <DropdownItem href="/investor/trading/orders">Orders</DropdownItem>
-                                    </DropdownMenu>
-                                </UncontrolledDropdown>
-
-                                <UncontrolledDropdown>
-                                    <DropdownToggle nav caret>
-                                        Funding
-                                    </DropdownToggle>
-
-                                    <DropdownMenu>
-                                        <DropdownItem href="/investor/funding/deposit">Deposit</DropdownItem>
-                                        <DropdownItem href="/investor/funding/withdrawal">Withdrawal</DropdownItem>
-                                        <DropdownItem href="/investor/funding/conversion">Funds Conversion</DropdownItem>
-                                    </DropdownMenu>
-                                </UncontrolledDropdown>
+                                <NavItem>
+                                    <NavLink href="/transactions">Transactions</NavLink>
+                                </NavItem>
                             </Nav>
 
-                            {/* search bar */}
-                            <Form inline className="mr-auto ml-auto mt-2 mt-md-0">
-                                <InputGroup className="input-group">
-                                    <Input type="text" placeholder="Search for..." />
-                                    <InputGroupAddon addonType="append">
-                                        <Button>Go!</Button>
-                                    </InputGroupAddon>
-                                </InputGroup>
-                            </Form>
-
                             {/* right menu */}
-                            <Nav navbar>
+                            <Nav navbar className="ml-auto">
                                 <UncontrolledDropdown>
                                     <DropdownToggle nav caret>
                                         Account
                                     </DropdownToggle>
 
                                     <DropdownMenu>
-                                        <DropdownItem href="/investor/settings">Settings</DropdownItem>
+                                        <DropdownItem href="/profile">Profile</DropdownItem>
                                         <DropdownItem divider />
-                                        <DropdownItem href="/auth/logout">Sign Out</DropdownItem>
+                                        <DropdownItem href="/logout">Sign Out</DropdownItem>
                                     </DropdownMenu>
                                 </UncontrolledDropdown>
                             </Nav>
