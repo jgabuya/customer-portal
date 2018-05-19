ca1a8c3223703d14f474b121819eb607b52c29d0
Jirffey Gabuya <boyhimas@gmail.com>
2018-05-19 16:23:33 +0800

    Minor change

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -10,7 +10,7 @@ router.get('/', function (req, res, next) {
 /* Mock user data update */
 router.post('/', function (req, res, next) {
     res.status(200).json({
-        success: true
+        message: 'Update successful'
     });
 });
 
